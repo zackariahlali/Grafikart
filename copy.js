d37795d5e73b1c44bbb0cf066108c60b1f7a154d
zakaria hlali <hlali.zackaria@gmail.com>
2017-11-23 16:49:53 +0100

    update project

--- copy.js
+++ copy.js
@@ -6,12 +6,13 @@ fs.stat(file, (err, stat) => {
     let total = stat.size
     let progress = 0
     let read = fs.createReadStream(file)
+    let write = fs.createWriteStream('copy.mp4')
     read.on('data', (chunk) => {
         progress += chunk.length
         console.log("J'ai lu  " + Math.round(100 * progress / total) + "%")
     })
-
-    read.on('end', () => {
-        console.log("J'ai tout finit !!")
+    read.pipe(write)
+    write.on('finish', () => {
+        console.log("Le fichier à bien été copié !")
     })
 })
